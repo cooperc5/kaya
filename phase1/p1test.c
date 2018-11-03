8b6cbfddf1f93192eae22def474ea25782f48c86
cooperc5 <cooperchris309@gmail.com>
2018-11-03 18:17:49 -0500

    testy testface

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -131,6 +131,7 @@ void main() {
 
 	/* Check allocProc */
 	for (i = 0; i < MAXPROC; i++) {
+		adderrbuf("Check allocProc["+i+"]   \n");
 		if ((procp[i] = allocPcb()) == NULL)
 			adderrbuf("allocPcb: unexpected NULL   ");
 	}
