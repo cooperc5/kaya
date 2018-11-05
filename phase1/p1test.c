64c953ac96ebda782ab0cc13b1063a0f20f1e613
cooperc5 <cooperchris309@gmail.com>
2018-11-05 13:48:40 -0500

    ;

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -195,8 +195,12 @@ void main() {
                 adderrbuf("headProcQ failed   ");
         q = outProcQ(&qa, firstproc);
         if (q == NULL || q != firstproc)
-        		if (q==NULL) {addokbuf("q=NULL")}
-        			if (q==NULL) {addokbuf("q!=firstproc")}
+        		if (q==NULL) 
+        			{addokbuf("q=NULL");
+        	}
+        			if (q==NULL) 
+        				{addokbuf("q!=firstproc");
+        		}
                 adderrbuf("outProcQ failed on first entry   ");
         freePcb(q);
         q = outProcQ(&qa, midproc);
