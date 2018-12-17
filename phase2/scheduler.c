6ae5c3115de33c390b5202b24403fcc4c20b0f70
cooperc5 <cooperchris309@gmail.com>
2018-12-16 20:44:34 -0500

    ughhhhhh

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -9,49 +9,52 @@ pcb_PTR runningProcess;
 cpu_t startTOD;
 cpu_t currentTOD;
 
-void debugScheduler(int a) {
-	int i;
-	i=0;
-}
-
-extern void scheduler() {
-	debugScheduler(13);
-	if (emptyProcQ(readyQueue)) {
-		currentProcess = NULL;
-		if (processCount == 0) {
-            	HALT();
-		}
-		if (processCount > 0) {
-			if (softBlockedCount == 0) {
-				PANIC();
-			}
-			if (softBlockedCount > 0) {
-				/* modify status */
-				setSTATUS(getSTATUS() | OFF | INTERRUPTSON | IEc | IM);
-				WAIT();
-			}
-		}
-	
-
-	} else {
-        /* use round robin for next job */
+void invokeScheduler() {
+    /* are there any ready jobs? */
+    if(emptyProcQ(readyQueue)) {
+        /* we have no running process */
+        currentProcess = NULL;
+        /* do we have any job to do? */
+        if(processCount == 0) {
+            /* nothing to do */
+            HALT();
+        }
+        /* are we waiting on I/O? */
+        if(processCount > 0) {
+            /* do are we waiting for I/O? */
+            if(softBlockedCount == 0) {
+                /* not a job, not waiting for a job, 
+                and are not a job itself - kernel panic */ 
+                PANIC();
+                /* are we waiting for I/O? */
+            } else if(softBlockedCount > 0) {
+                /* enable interrupts for the next job */
+                setSTATUS(getSTATUS() | ALLOFF | INTERRUPTSON | IEc | IM);
+                /* wait */
+                WAIT();
+            }
+        }
+    } else {
+        /* simply ready the next job using round-robbin */
         if (currentProcess != NULL) {
             /* start the clock */
             STCK(currentTOD);
             currentProcess->p_time = currentProcess->p_time + (currentTOD - startTOD);
         }
-        /* set interrupt timer */
+        /* generate an interrupt when timer is up */
         if(currentTOD < QUANTUM) {
+            /* our current job will be less than 
+            our quantum, take the shorter */
             setTIMER(currentTOD);
         } else {
             /* set the quantum */
             setTIMER(QUANTUM);
         }
-        /* pop a job */
+        /* grab a job */
         currentProcess = removeProcQ(&(readyQueue));
         STCK(startTOD);
-        
-        LDST(&(currentProcess->p_s));
-	}
+        /* perform a context switch */
+        contextSwitch(&(currentProcess->p_s));
+    }
 }
 
